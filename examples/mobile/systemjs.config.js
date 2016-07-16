57c82e9cd7f5b4c1116b0ccf7fb8fbc96508df7a
KeatonTech <keaton.brandt@gmail.com>
2016-07-16 15:39:33 -0400

    Maybe fix gh-pages node-modules

--- examples/mobile/systemjs.config.js
+++ examples/mobile/systemjs.config.js
@@ -6,9 +6,9 @@
   // map tells the System loader where to look for things
   var map = {
     'examples/mobile/app':          '/examples/build.js',
-    '@angular':                     '/node_modules/@angular',
-    'angular2-in-memory-web-api':   '/node_modules/angular2-in-memory-web-api',
-    'rxjs':                         '/node_modules/rxjs'
+    '@angular':                     '../../node_modules/@angular',
+    'angular2-in-memory-web-api':   '../../node_modules/angular2-in-memory-web-api',
+    'rxjs':                         '../../node_modules/rxjs'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
